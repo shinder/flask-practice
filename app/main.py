bd9d910772f77ecaa222628d52da17364523edf1
shinder <shinder.lin@gmail.com>
2020-04-19 15:56:54 +0800

    靜態檔案資料夾

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import modules.functions
 # __name__ 用來 application 的相對位置
 # 若是直接啟動的程式 __name__ 為 '__main__'
 # 若是被滙入， __name__ 會是被滙入的名稱
-app = Flask(__name__)
+app = Flask(__name__, '/')
 
 
 # decorators 後面定義的 function 會變成 decorators 的參數
